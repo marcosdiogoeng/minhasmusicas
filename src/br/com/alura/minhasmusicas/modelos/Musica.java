49e46d34bdf705a88a4a360e38d4f7b0d9ef8697
marcosd <marcosdiogo.eng@outlook.com>
2023-05-21 11:14:05 -0300

    push

--- src/br/com/alura/minhasmusicas/modelos/Musica.java
+++ src/br/com/alura/minhasmusicas/modelos/Musica.java
@@ -1,4 +1,7 @@
 package br.com.alura.minhasmusicas.modelos;
 
-public class Musica {
+public class Musica extends Audio{
+    private String album;
+    private String cantor;
+    private String genero;
 }
